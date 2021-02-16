3d5ab2a2dd07f54a71b668aae9f32cb7723289e5
Tara78 <70331037+Tara78@users.noreply.github.com>
2021-02-16 08:17:09 +0100

    Update server.js
    api

--- nodejs-express-api-example/server.js
+++ nodejs-express-api-example/server.js
@@ -1,16 +1,38 @@
 const fs = require('fs'); 
 const express = require('express'); 
+
 // skapa en server from express
 const server= express(); 
+
 
 server.get('/books', (request, response)=>{
 fs.readFile('childrensbooks.json', 'utf8', (error, content)=>{
     response.send(content); 
+}); 
+
 })
-})
+
+server.get('/books/:id', (request, response) => {
+    console.log('Id är: ', request.params.id);
+    //Hämta id från url:en och spara i en variabel
+    const id = request.params.id;
+    //Läs in childrensbooks.json
+    fs.readFile('childrensbooks.json', 'utf8', (error, content) => {
+        //Gör om till JSON för att kunna loopa igenom
+        const books = JSON.parse(content);
+        //Använd arraymetoden filter för att loopa igenom och returnera ett matchande objekt
+        const foundBook = books.filter((book) => {
+            return book.id === parseInt(id);
+        });
+        //Skicka tillbaka det hittade objektet
+        response.send(foundBook[0]);
+    });
+});
+
 
 
 // server lyssnar pa en spesifik port( i detta fall 7000)
 server.listen (7000, ()=>{
     console.log('Server started on http://localhost:7000'); 
+ 
 });
